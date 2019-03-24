2137b5c5576a4bf3e83c3422b49b2c54bc8830c1
devnokiyo <devnokiyo@gmail.com>
2019-03-24 22:04:40 +0900

    AppディレクトリにRailsプロジェクトがあることを前提としたDocker Composeの構成に変更

--- backend/docker/containers/rails/docker-entrypoint.sh
+++ backend/docker/containers/rails/docker-entrypoint.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
 set -e
-
-if [ ! -e Gemfile ]; then
-  cp -a $READY_RAILS_DIR/Gemfile .
-  touch Gemfile.lock
-
-  bundle install --path vendor/bundle
-  bundle exec rails new . -d mysql -f -T --api --skip-bundle
-
-  cp -a $READY_RAILS_DIR/database.yml $READY_RAILS_DIR/puma.rb config/
-  cp -a $READY_RAILS_DIR/db_charset_utf8mb4.rb config/initializers/
-
+if [ ! -e vendor ]; then
   mkdir -p tmp/sockets
-
-  rm -r $READY_RAILS_DIR
-
-  rm -rf .bundle
-  rm -rf vendor/bundle
   bundle install --path vendor/bundle
 
   until bundle exec rails db:drop &> /dev/null; do
@@ -26,10 +11,10 @@ if [ ! -e Gemfile ]; then
   done
 
   bundle exec rails db:create
-
-elif [ -e $READY_RAILS_DIR ]; then
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
+else
   bundle install --path vendor/bundle
-  rm -r $READY_RAILS_DIR
 fi
 
 exec "$@"
